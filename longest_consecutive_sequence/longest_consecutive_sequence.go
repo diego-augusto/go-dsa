8b5c2f4f175d78ac35ff1acb37e6655224ee5dc8
Diego Augusto <diego.moura@mheducation.com>
2023-12-08 21:22:41 -0300

    enhance longest consecutive sequence

--- longest_consecutive_sequence/longest_consecutive_sequence.go
+++ longest_consecutive_sequence/longest_consecutive_sequence.go
@@ -2,26 +2,32 @@ package longestconsecutivesequence
 
 func longestConsecutive(nums []int) int {
 
-	m := make(map[int]int)
-
+	//create a set
+	set := make(map[int]struct{})
 	for _, n := range nums {
-		m[n]++
+		set[n] = struct{}{}
 	}
 
 	longest := 0
-
 	for _, n := range nums {
-		if _, ok := m[n-1]; !ok {
+
+		//if number does not have a previous, it is the beginning of the sequence
+		if _, ok := set[n-1]; !ok {
 			count := 1
-			for {
-				if _, ok := m[n+count]; !ok {
-					break
-				} else {
-					count++
-				}
+
+			//try to find next number of the sequence
+			for _, ok := set[n+count]; ok; _, ok = set[n+count] {
+				count++
 			}
+
 			if count > longest {
 				longest = count
+			}
+
+			//if the current sequence is greather than half of len(nums),
+			//the next sequence cant be greather than current one.
+			if count > len(nums)/2 {
+				break
 			}
 		}
 	}
