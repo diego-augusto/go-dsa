7ccc3ed101b31629d247534b0b0ba4d7331cdd11
Diego Augusto <diego.moura@mheducation.com>
2024-03-30 00:48:03 -0300

    reverselinkedlist

---
+++ reverse_linked_list/reverse_linked_list_test.go
@@ -0,0 +1,27 @@
+package reverselinkedlist
+
+import "testing"
+
+func Test(t *testing.T) {
+
+	head := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val: 3,
+				Next: &ListNode{
+					Val: 4,
+					Next: &ListNode{
+						Val:  5,
+						Next: nil,
+					},
+				},
+			},
+		},
+	}
+
+	reverseList(head)
+
+	_ = head
+}
